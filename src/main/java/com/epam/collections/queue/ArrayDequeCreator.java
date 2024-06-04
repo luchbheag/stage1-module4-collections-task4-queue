67004a8a347ec2fd49d7f25d1293ded1eca841d9
luchbheag <nedolivko.julia@gmail.com>
2024-06-04 15:58:43 +0200

    do queue task

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,20 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> deck = new ArrayDeque<>();
+        deck.add(firstQueue.poll());
+        deck.add(firstQueue.poll());
+        deck.add(secondQueue.poll());
+        deck.add(secondQueue.poll());
+        while (!firstQueue.isEmpty()) {
+            firstQueue.add(deck.pollLast());
+            deck.add(firstQueue.poll());
+            deck.add(firstQueue.poll());
+
+            secondQueue.add(deck.pollLast());
+            deck.add(secondQueue.poll());
+            deck.add(secondQueue.poll());
+        }
+        return deck;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,29 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> dishes = new ArrayList<>();
+        if (numberOfDishes == 0)
+            return dishes;
+
+        LinkedList<Integer> menu = new LinkedList<>();
+        for (int i = 0; i < numberOfDishes; i++) {
+            menu.add(i);
+        }
+        int index = everyDishNumberToEat - 1;
+        while (!menu.isEmpty()) {
+            if (index >= menu.size()) {
+                index %= menu.size();
+            } else {
+                dishes.add(menu.get(index) + 1);
+                menu.remove(index);
+                index += everyDishNumberToEat - 1;
+            }
+        }
+        return dishes;
     }
 }
